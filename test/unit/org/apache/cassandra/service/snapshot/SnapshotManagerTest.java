82acae74e4eb904cbf47be5eb8478bd6fd437bd2
fibersel <0583463@gmail.com>
2021-07-25 00:25:38 +0300

    add testing flow for SnapshotManager, add shutdown of snapshot manager

--- test/unit/org/apache/cassandra/service/snapshot/SnapshotManagerTest.java
+++ test/unit/org/apache/cassandra/service/snapshot/SnapshotManagerTest.java
@@ -18,6 +18,71 @@
 
 package org.apache.cassandra.service.snapshot;
 
-public class SnapshotManagerTest
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.function.Predicate;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.apache.cassandra.OrderedJUnit4ClassRunner;
+import org.apache.cassandra.cql3.CQLTester;
+import org.apache.cassandra.service.StorageService;
+import org.apache.cassandra.tools.NodeProbe;
+import org.apache.cassandra.tools.ToolRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(OrderedJUnit4ClassRunner.class)
+public class SnapshotManagerTest extends CQLTester
 {
+    private static NodeProbe probe;
+
+    private static void SSstart() throws Exception {
+        StorageService.instance.initServer();
+        startJMXServer();
+        probe = new NodeProbe(jmxHost, jmxPort);
+    }
+
+    @BeforeClass
+    public static void setup() throws Exception
+    {
+        SSstart();
+    }
+
+    @AfterClass
+    public static void teardown() throws IOException
+    {
+        probe.close();
+    }
+
+    @Test
+    public void testSnapshotIndexingAndRemoval() throws InterruptedException
+    {
+        ToolRunner.ToolResult tool = ToolRunner.invokeNodetool("snapshot", "--ttl", "1m", "-t", "some-name");
+
+        PriorityQueue<TableSnapshotDetails> snapshots = StorageService.instance.snapshotManager.getExpiringSnapshots();
+        Predicate<TableSnapshotDetails> tagPredicate = (details) -> details.getTag().equals("some-name");
+        assertThat(snapshots.parallelStream().filter(tagPredicate).count()).isNotEqualTo(0L);
+
+        Thread.sleep(70000);
+
+        assertThat(snapshots.parallelStream().filter(tagPredicate).count()).isEqualTo(0L);
+    }
+
+    @Test
+    public void testSnapshotsLoadOnStartup() throws Exception {
+        ToolRunner.ToolResult tool = ToolRunner.invokeNodetool("snapshot", "--ttl", "10m", "-t", "some-name");
+
+        PriorityQueue<TableSnapshotDetails> snapshots = StorageService.instance.snapshotManager.getExpiringSnapshots();
+        Predicate<TableSnapshotDetails> tagPredicate = (details) -> details.getTag().equals("some-name");
+        assertThat(snapshots.parallelStream().filter(tagPredicate).count()).isNotEqualTo(0L);
+
+        StorageService.instance.snapshotManager.shutdown();
+        StorageService.instance.snapshotManager.start();
+
+        assertThat(snapshots.parallelStream().filter(tagPredicate).count()).isNotEqualTo(0L);
+    }
 }
